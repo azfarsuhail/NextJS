dfaa9c8b6bf3296dc3ab8bb5fe56ae4545d16bfd
Azfar Suhail <138823481+azfarsuhail@users.noreply.github.com>
2023-12-12 20:08:19 +0500

    Update page.tsx

--- Google/src/app/About/page.tsx
+++ Google/src/app/About/page.tsx
@@ -1,82 +1,98 @@
-
 export default function About() {
-    return (
-        <div className="flex-wrap mt-6 gap-3 m-3 p-3">
- <h1 className="font-bold">About Google</h1>
-<br />
-<h2>Introduction</h2>
+  return (
+    <div className="flex-wrap mt-6 gap-3 m-3 p-3">
+      <h1 className="font-bold">About Google</h1>
+      <br />
+      <h2>Introduction</h2>
 
-<p>
-  Google, a multinational technology company, is a global leader in
-  internet-related services and products. Founded in 1998 by Larry Page
-  and Sergey Brin while they were Ph.D. students at Stanford University,
-  Google has evolved into one of the most influential and innovative
-  companies in the world.
-</p>
+      <p>
+        Google, a multinational technology company, is a global leader in
+        internet-related services and products. Founded in 1998 by Larry Page
+        and Sergey Brin while they were Ph.D. students at Stanford University,
+        Google has evolved into one of the most influential and innovative
+        companies in the world.
+      </p>
 
-<h2>Mission and Vision</h2>
-<p>
-  Google's mission is to organize the world's information and make it
-  universally accessible and useful. This commitment reflects in their
-  diverse range of products and services designed to enhance user
-  experience, simplify tasks, and connect people across the globe.
-</p>
+      <h2>Mission and Vision</h2>
+      <p>
+        Google&apos;s mission is to organize the world&apos;s information and make it
+        universally accessible and useful. This commitment reflects in their
+        diverse range of products and services designed to enhance user
+        experience, simplify tasks, and connect people across the globe.
+      </p>
 
-<h2>Core Products and Services</h2>
-<ul>
-  <li>
-    1. Search Engine: Google's search engine is at the core of its
-    success, providing users with a powerful and efficient tool to find
-    information on the internet.
-  </li>
-  <li>
-    2. Advertising: Google's advertising platform, including Google Ads
-    and AdSense, plays a pivotal role in the digital marketing landscape,
-    allowing businesses to reach their target audiences effectively.
-  </li>
-  <li>
-    3. Gmail: Launched in 2004, Gmail has become one of the most popular
-    email services globally, known for its intuitive interface, powerful
-    features, and substantial storage capacity.
-  </li>
-  <li>
-    4. Google Maps: Google Maps revolutionized navigation and
-    location-based services, offering detailed maps, real-time traffic
-    information, and local business reviews.
-  </li>
-  <li>
-    5. YouTube: Acquired by Google in 2006, YouTube is the world's
-    largest video-sharing platform, providing a space for creators to
-    share and users to discover a vast array of content.
-  </li>
-  <li>
-    6. Android: Google's mobile operating system, Android, powers a
-    significant portion of the world's smartphones, offering an
-    open-source platform for developers.
-  </li>
-  <li>
-    7. Chrome Browser: The Chrome browser, known for its speed and
-    simplicity, is one of the most widely used web browsers globally.
-  </li>
-</ul>
+      <h2>Core Products and Services</h2>
+      <ul>
+        <li>
+          1. Search Engine: Google&apos;s search engine is at the core of its
+          success, providing users with a powerful and efficient tool to find
+          information on the internet.
+        </li>
+        <li>
+          2. Advertising: Google&apos;s advertising platform, including Google Ads
+          and AdSense, plays a pivotal role in the digital marketing landscape,
+          allowing businesses to reach their target audiences effectively.
+        </li>
+        <li>
+          3. Gmail: Launched in 2004, Gmail has become one of the most popular
+          email services globally, known for its intuitive interface, powerful
+          features, and substantial storage capacity.
+        </li>
+        <li>
+          4. Google Maps: Google Maps revolutionized navigation and
+          location-based services, offering detailed maps, real-time traffic
+          information, and local business reviews.
+        </li>
+        <li>
+          5. YouTube: Acquired by Google in 2006, YouTube is the world&apos;s
+          largest video-sharing platform, providing a space for creators to
+          share and users to discover a vast array of content.
+        </li>
+        <li>
+          6. Android: Google&apos;s mobile operating system, Android, powers a
+          significant portion of the world&apos;s smartphones, offering an
+          open-source platform for developers.
+        </li>
+        <li>
+          7. Chrome Browser: The Chrome browser, known for its speed and
+          simplicity, is one of the most widely used web browsers globally.
+        </li>
+      </ul>
 
-<h2>Innovation and Research</h2>
-<p>
-  Google has consistently been at the forefront of technological
-  innovation. The company invests heavily in research and development,
-  leading to groundbreaking projects such as Google Glass, self-driving
-  cars through Waymo, and the development of artificial intelligence
-  technologies.
-</p>
+      <h2>Innovation and Research</h2>
+      <p>
+        Google has consistently been at the forefront of technological
+        innovation. The company invests heavily in research and development,
+        leading to groundbreaking projects such as Google Glass, self-driving
+        cars through Waymo, and the development of artificial intelligence
+        technologies.
+      </p>
 
-<h2> Corporate Culture</h2>
-Google is renowned for its unique corporate culture, emphasizing innovation, creativity, and a workplace that encourages employees to think outside the box. The company's headquarters, known as the Googleplex, reflects this culture with its unconventional workspaces and recreational amenities.
+      <h2> Corporate Culture</h2>
+      <p>
+        Google is renowned for its unique corporate culture, emphasizing innovation,
+        creativity, and a workplace that encourages employees to think outside the box.
+        The company&apos;s headquarters, known as the Googleplex, reflects this culture
+        with its unconventional workspaces and recreational amenities.
+      </p>
 
-<h2> Commitment to Sustainability</h2>
-Google is committed to environmental sustainability. The company has set ambitious goals to operate entirely on carbon-free energy and to be water-neutral. Initiatives include investments in renewable energy projects and the development of eco-friendly data centers.
+      <h2> Commitment to Sustainability</h2>
+      <p>
+        Google is committed to environmental sustainability. The company has set
+        ambitious goals to operate entirely on carbon-free energy and to be water-neutral.
+        Initiatives include investments in renewable energy projects and the development
+        of eco-friendly data centers.
+      </p>
 
-<h2> Conclusion</h2>
-Google's impact on the digital landscape is immeasurable. From its humble beginnings as a search engine to its current status as a tech giant with a diverse portfolio, Google continues to shape the way we access information, connect with others, and experience the online world. With a commitment to innovation, user-centric products, and a forward-thinking approach, Google remains a driving force in the evolution of technology.
-</div>
-    );
+      <h2> Conclusion</h2>
+      <p>
+        Google&apos;s impact on the digital landscape is immeasurable. From its humble
+        beginnings as a search engine to its current status as a tech giant with a
+        diverse portfolio, Google continues to shape the way we access information,
+        connect with others, and experience the online world. With a commitment to
+        innovation, user-centric products, and a forward-thinking approach, Google remains
+        a driving force in the evolution of technology.
+      </p>
+    </div>
+  );
 }
