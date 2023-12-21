55ebfacca1d544b3f5393c14a1c4e21d258e466f
Azfar Suhail <138823481+azfarsuhail@users.noreply.github.com>
2023-12-21 21:45:02 +0500

    Update company name in Footer

--- Google/src/app/Footer/page.tsx
+++ Google/src/app/Footer/page.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
       <footer className="bg-gray-800 text-white p-4 bottom-0">
         <div className="container mx-auto">
-          <p className="text-center">© 2023 Your Company. All rights reserved.</p>
+          <p className="text-center">© 2023 Azfar Suhail. All rights reserved.</p>
         </div>
       </footer>
     );
