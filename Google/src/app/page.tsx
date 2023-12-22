cc8c0b91c4e147ef9e2a4961e8425cf410e77fd4
Azfar Suhail <138823481+azfarsuhail@users.noreply.github.com>
2023-12-22 05:21:31 +0500

    Solving Leakage issue of Main Div

--- Google/src/app/page.tsx
+++ Google/src/app/page.tsx
@@ -3,7 +3,7 @@ import logo from '../../public/logo.png'
 
 export default function Navbar() {
   return(
-    <div className='flex order-2  justify-center items-center'>
+    <div className='flex-col md:flex order-2  justify-center items-center'>
       <Image src={logo} alt="logo" className=" w-23 h-23"/>
         <h1>Welcome to Google</h1>
     </div>
